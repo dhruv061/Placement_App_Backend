da269ab8595f9931be6b6c49e9f2f4f1fd1f6a99
Dhruv Mavani <96719397+dhruv061@users.noreply.github.com>
2023-08-26 17:16:26 +0530

    API updated 2

--- model/user.js
+++ model/user.js
@@ -15,62 +15,78 @@ const HomeScreenSchema = mongoose.Schema({
     },
   });
 
-  const CompanySchema = mongoose.Schema({
-    title: {
-      required: true,
-      type: String,
-    },
-    date: {
-      required: true,
-      type: String,
-    },
-    description: {
-      required: true,
-      type: String,
-    },
-  });
+const CompanySchema = mongoose.Schema({
+  title: {
+    required: true,
+    type: String,
+  },
+  date: {
+    required: true,
+    type: String,
+  },
+  description: {
+    required: true,
+    type: String,
+  },
+  package: {
+    required: true,
+    type: String,
+  },
+  location: {
+    required: true,
+    type: String,
+  },
+  bound: {
+    required: true,
+    type: String,
+  },
+  link: {
+    required: true,
+    type: String,
+  },
+});
 
-  const Login7CESchema = mongoose.Schema({
-    email: {
-      required: true,
-      type: String,
-    },
-    password: {
-      required: true,
-      type: String,
-    },
-    branch: {
-      required: true,
-      type: String,
-    },
-  });
+const Login7CESchema = mongoose.Schema({
+  email: {
+    required: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+  branch: {
+    required: true,
+    type: String,
+  },
+});
 
-  const Login7ITSchema = mongoose.Schema({
-    email: {
-      required: true,
-      type: String,
-    },
-    password: {
-      required: true,
-      type: String,
-    },
-    branch: {
-      required: true,
-      type: String,
-    },
-  });
-
-
-  const HomeScreen = mongoose.model("Home Screen Data", HomeScreenSchema);
-  const Login7CE = mongoose.model("Userdata 7CE", Login7CESchema);
-  const Login7IT = mongoose.model("Userdata 7IT", Login7ITSchema);
-  const Company = mongoose.model("Company Data", CompanySchema);
+const Login7ITSchema = mongoose.Schema({
+  email: {
+    required: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+  branch: {
+    required: true,
+    type: String,
+  },
+});
 
 
-  module.exports = {
-    HomeScreen,
-    Login7CE,
-    Login7IT,
-    Company,
-  };
+const HomeScreen = mongoose.model("Home Screen Data", HomeScreenSchema);
+const Login7CE = mongoose.model("Userdata 7CE", Login7CESchema);
+const Login7IT = mongoose.model("Userdata 7IT", Login7ITSchema);
+const Company = mongoose.model("Company Data", CompanySchema);
+
+
+module.exports = {
+  HomeScreen,
+  Login7CE,
+  Login7IT,
+  Company,
+};
   
